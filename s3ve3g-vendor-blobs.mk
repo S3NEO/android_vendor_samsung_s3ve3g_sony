ba625fa6d27a1b7a532e286a69e8183ea62d9d1b
PythonLimited <leonardomeitz@gmail.com>
2018-01-08 11:53:20 +0100

    update winedevine libs to los-14.1

--- s3ve3g-vendor-blobs.mk
+++ s3ve3g-vendor-blobs.mk
@@ -139,18 +139,18 @@ PRODUCT_COPY_FILES += \
 	vendor/samsung/s3ve3g/proprietary/cameradata/datapattern_420sp.yuv:system/cameradata/datapattern_420sp.yuv \
 	vendor/samsung/s3ve3g/proprietary/cameradata/datapattern_front_420sp.yuv:system/cameradata/datapattern_front_420sp.yuv \
 	vendor/samsung/s3ve3g/proprietary/bin/qseecomd:system/bin/qseecomd \
-	vendor/samsung/s3ve3g/proprietary/lib/drm/libdrmwvmplugin.so:system/lib/drm/libdrmwvmplugin.so \
+	vendor/samsung/s3ve3g/proprietary/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
 	vendor/samsung/s3ve3g/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
-	vendor/samsung/s3ve3g/proprietary/lib/libdrmdecrypt.so:system/lib/libdrmdecrypt.so \
-	vendor/samsung/s3ve3g/proprietary/vendor/lib/libdrmdiag.so:system/vendor/lib/libdrmdiag.so \
-	vendor/samsung/s3ve3g/proprietary/vendor/lib/libdrmfs.so:system/vendor/lib/libdrmfs.so \
-	vendor/samsung/s3ve3g/proprietary/vendor/lib/libdrmtime.so:system/vendor/lib/libdrmtime.so \
-	vendor/samsung/s3ve3g/proprietary/vendor/lib/libQSEEComAPI.so:system/vendor/lib/libQSEEComAPI.so \
-	vendor/samsung/s3ve3g/proprietary/vendor/lib/librpmb.so:system/vendor/lib/librpmb.so \
-	vendor/samsung/s3ve3g/proprietary/vendor/lib/libssd.so:system/vendor/lib/libssd.so \
-	vendor/samsung/s3ve3g/proprietary/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
-	vendor/samsung/s3ve3g/proprietary/lib/libwvm.so:system/lib/libwvm.so \
-	vendor/samsung/s3ve3g/proprietary/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so \
+	vendor/samsung/s3ve3g/proprietary/vendor/lib/libdrmdecrypt.so:system/vendor/lib/libdrmdecrypt.so \
+	vendor/samsung/s3ve3g/proprietary/lib/libdrmdiag.so:system/vendor/lib/libdrmdiag.so \
+	vendor/samsung/s3ve3g/proprietary/lib/libdrmfs.so:system/vendor/lib/libdrmfs.so \
+	vendor/samsung/s3ve3g/proprietary/lib/libdrmtime.so:system/vendor/lib/libdrmtime.so \
+	vendor/samsung/s3ve3g/proprietary/lib/libQSEEComAPI.so:system/vendor/lib/libQSEEComAPI.so \
+	vendor/samsung/s3ve3g/proprietary/lib/librpmb.so:system/vendor/lib/librpmb.so \
+	vendor/samsung/s3ve3g/proprietary/lib/libssd.so:system/vendor/lib/libssd.so \
+	vendor/samsung/s3ve3g/proprietary/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
+	vendor/samsung/s3ve3g/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
+	vendor/samsung/s3ve3g/proprietary/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
 	vendor/samsung/s3ve3g/proprietary/lib/hw/flp.default.so:system/lib/hw/flp.default.so \
 	vendor/samsung/s3ve3g/proprietary/lib/hw/gps.msm8226.so:system/lib/hw/gps.msm8226.so \
 	vendor/samsung/s3ve3g/proprietary/lib/libloc_api_v02.so:system/lib/libloc_api_v02.so \
