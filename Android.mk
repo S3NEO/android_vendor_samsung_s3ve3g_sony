05f1149868589c6ea0765d38d6066629977a74e1
PythonLimited <Leonardomeitz@gmail.com>
2018-12-26 20:56:52 +0100

    update time_service and delete mpdecision

--- Android.mk
+++ Android.mk
@@ -18,5 +18,16 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),s3ve3g)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := TimeService
+LOCAL_MODULE_OWNER := samsung
+LOCAL_SRC_FILES := proprietary/app/TimeService/TimeService.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+include $(BUILD_PREBUILT)
+
 endif
 
