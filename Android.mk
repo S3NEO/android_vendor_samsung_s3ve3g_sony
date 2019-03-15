a53ff09ce475332513218896f332a69fc67d0722
PythonLimited <Leonardomeitz@gmail.com>
2019-03-15 21:35:38 +0100

    use correct path

--- Android.mk
+++ Android.mk
@@ -21,7 +21,7 @@ ifeq ($(TARGET_DEVICE),s3ve3g)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libtime_genoff
 LOCAL_MODULE_OWNER := samsung
-LOCAL_SRC_FILES := proprietary/vendor/lib/libtime_genoff.so
+LOCAL_SRC_FILES := proprietary/lib/libtime_genoff.so
 LOCAL_MULTILIB := 32
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
